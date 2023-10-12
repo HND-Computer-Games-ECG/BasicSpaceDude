ab61f17043dbbecdd481627d809da85e2b0bb32d
Nigel Kennington <nigel.kennington@edinburghcollege.ac.uk>
2023-10-12 09:25:45 +0100

    Revert "Reimplemented dudes using lists"
    This reverts commit 8ef35abedf32d2da38e7be875d7d3ee20551fdca.

--- BasicSpaceDude/Game1.cs
+++ BasicSpaceDude/Game1.cs
@@ -1,8 +1,6 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
-using Microsoft.Xna.Framework.Input;
 using System;
-using System.Collections.Generic;
 
 namespace BasicSpaceDude
 {
@@ -22,9 +20,7 @@ namespace BasicSpaceDude
 
         // Create a variable called "dudes" that is an array (collection) of
         // variables of type FloatyDude
-        // FloatyDude[] dudes;
-        List<FloatyDude> dudes;
-        int nextDudeColour;
+        FloatyDude[] dudes;
 
         public Game1()
         {
@@ -36,10 +32,7 @@ namespace BasicSpaceDude
         protected override void Initialize()
         {
             // Specify that dudes has space for 100 dudes
-            // dudes = new FloatyDude[NUMBEROFDUDES];
-            dudes = new List<FloatyDude>();
-            nextDudeColour = 0;
-
+            dudes = new FloatyDude[NUMBEROFDUDES];
             screenCenter = GraphicsDevice.Viewport.Bounds.Center.ToVector2();
 
             base.Initialize();
@@ -51,32 +44,18 @@ namespace BasicSpaceDude
 
             bg = new Background(Content.Load<Texture2D>("starfield"));
 
-            //for (int i = 0; i < dudes.Length; i++)
-            //{
-            //    Vector2 startPos = screenCenter + new Vector2(RNG.Next(-100, 100),RNG.Next(-100, 100));
-            //    Vector2 startVel = new Vector2((float)(RNG.NextDouble() * 2) - 1,
-            //                                            (float)(RNG.NextDouble() * 2) - 1);
-            //    dudes[i] = new FloatyDude(Content.Load<Texture2D>("dude" + (i%6)), startPos, startVel);
-            //}
+            for (int i = 0; i < dudes.Length; i++)
+            {
+                Vector2 startPos = screenCenter + new Vector2(RNG.Next(-100, 100),RNG.Next(-100, 100));
+                Vector2 startVel = new Vector2((float)(RNG.NextDouble() * 2) - 1,
+                                                        (float)(RNG.NextDouble() * 2) - 1);
+                dudes[i] = new FloatyDude(Content.Load<Texture2D>("dude" + (i%6)), startPos, startVel);
+            }
         }
 
         protected override void Update(GameTime gameTime)
         {
-            Vector2 startPos = screenCenter + new Vector2(RNG.Next(-100, 100), RNG.Next(-100, 100));
-            Vector2 startVel = new Vector2((float)(RNG.NextDouble() * 2) - 1,
-                                                    (float)(RNG.NextDouble() * 2) - 1);
-
-            if (Mouse.GetState().RightButton == ButtonState.Pressed)
-            {
-                dudes.Add(new FloatyDude(Content.Load<Texture2D>("dude"+nextDudeColour),
-                                            startPos, startVel));
-                nextDudeColour++;
-                if (nextDudeColour > 5)
-                    nextDudeColour = 0;
-            }
-
-
-            for (int i = 0; i < dudes.Count; i++)
+            for (int i = 0; i < dudes.Length; i++)
                 dudes[i].UpdateMe(GraphicsDevice.Viewport.Bounds);
 
             base.Update(gameTime);
@@ -89,7 +68,7 @@ namespace BasicSpaceDude
             _spriteBatch.Begin();
             bg.DrawMe(_spriteBatch);
 
-            for (int i = 0; i < dudes.Count; i++)
+            for (int i = 0; i < dudes.Length; i++)
                 dudes[i].DrawMe(_spriteBatch);
 
             _spriteBatch.End();
